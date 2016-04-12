210b53e565f21b60458f54a297823622cd938828
James G Lee <jgl021@ieng6-203.ucsd.edu>
2016-04-12 13:44:16 -0700

    Added starter code for p1 and some crap im not sure is correct LMAO

--- assignment1_p1.py
+++ assignment1_p1.py
@@ -1,1 +1,26 @@
 __author__ = 'asauceda@ucsd.edu,A10482838,jgl021@ucsd.edu,A11380076'
+
+# Dict to keep track of seen primes
+seen = {}
+
+def getPossibleActions(currentPrime):
+  # This method would return the list of prime
+  # numbers reachable from the current prime.
+  # Note - this should not include the prime numbers
+  # which have already been processed, either in the
+  # frontier or in the closed list.
+  global seen
+  listOfPrimes = []
+  listOfPrimes
+  return listOfPrimes
+
+def getPath(startingPrime,finalPrime):
+  global seen
+  return path
+
+def main():
+  primes = str(sys.stdin.readline()).split()
+  print(getPath(primes[0],primes[1]))
+
+if __name__ == '__main__':
+  main()
